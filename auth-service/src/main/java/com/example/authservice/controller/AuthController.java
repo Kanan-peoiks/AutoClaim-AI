2cd0000bd6aed436f0b4345ab1a07db0523fd7b6
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-02-19 03:05:37 +0400

    feat: complete auth-service controller implementations

--- auth-service/src/main/java/com/example/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/example/authservice/controller/AuthController.java
@@ -1,9 +1,34 @@
 package com.example.authservice.controller;
 
+
+import com.example.authservice.dto.AuthRequest;
+import com.example.authservice.dto.AuthResponse;
+import com.example.authservice.dto.RegisterRequest;
+import com.example.authservice.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody RegisterRequest req) {
+        authService.register(req);
+        return ResponseEntity.status(HttpStatus.CREATED).body("User registered");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest req) {
+        AuthResponse res = authService.login(req);
+        return ResponseEntity.ok(res);
+    }
 }
+
