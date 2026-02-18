8fa928f8f266a2913bf0d7eeef97f207c8daa508
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-02-18 23:43:36 +0400

    add controllers for testing

---
+++ pricing-service/src/main/java/com/example/pricingservice/controller/PricingController.java
@@ -0,0 +1,15 @@
+package com.example.pricingservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/pricing")
+public class PricingController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "Pricing test";
+    }
+}
