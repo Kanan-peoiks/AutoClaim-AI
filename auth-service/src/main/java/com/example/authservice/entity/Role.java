81aec1424340e572d6ee46ebd2891ad8befd2b1c
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-02-19 02:52:34 +0400

    feat: complete auth-service entity implementations

--- auth-service/src/main/java/com/example/authservice/entity/Role.java
+++ auth-service/src/main/java/com/example/authservice/entity/Role.java
@@ -1,4 +1,6 @@
 package com.example.authservice.entity;
 
 public enum Role {
+    ROLE_ADMIN,
+    ROLE_USER
 }
--- auth-service/src/main/java/com/example/authservice/entity/UserEntity.java
+++ auth-service/src/main/java/com/example/authservice/entity/UserEntity.java
@@ -1,4 +1,31 @@
 package com.example.authservice.entity;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "users")
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
 }
