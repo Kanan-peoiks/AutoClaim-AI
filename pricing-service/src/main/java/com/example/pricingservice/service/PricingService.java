02a0ee7059eda09bfcd233093fdb0005c7851c3b
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-02-19 04:41:16 +0400

    feat: complete pricing-service service implementations

--- pricing-service/src/main/java/com/example/pricingservice/service/PricingService.java
+++ pricing-service/src/main/java/com/example/pricingservice/service/PricingService.java
@@ -1,4 +1,20 @@
 package com.example.pricingservice.service;
 
+import com.example.pricingservice.entity.PartPrice;
+import com.example.pricingservice.repository.PartPriceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class PricingService {
+    private final PartPriceRepository partPriceRepository;
+
+    public double getPrice(String partName) {
+
+        return partPriceRepository.findByPartName(partName)
+                .map(PartPrice::getPrice)
+                .orElse(0.0);
+    }
+
 }
