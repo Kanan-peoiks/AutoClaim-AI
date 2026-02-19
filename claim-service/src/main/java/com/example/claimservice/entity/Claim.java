72235f7811db6ff4b319d6331ce035067b3c944d
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-02-19 06:13:01 +0400

    feat: complete claim-service entity implementations

--- claim-service/src/main/java/com/example/claimservice/entity/Claim.java
+++ claim-service/src/main/java/com/example/claimservice/entity/Claim.java
@@ -1,4 +1,23 @@
 package com.example.claimservice.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection = "claims")
+@Data
+@AllArgsConstructor
+@Builder
 public class Claim {
+    @Id
+    private String id;
+    private String username;
+    private String photoUrl;
+    private List<String> damagedParts;
+    private double totalCost;
+    private String jsonData;
 }
