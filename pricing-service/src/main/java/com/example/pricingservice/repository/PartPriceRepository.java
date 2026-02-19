daf1f87e71f9677235198addc7e6a3a9c400624c
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-02-19 04:40:46 +0400

    feat: complete pricing-service repository implementations

--- pricing-service/src/main/java/com/example/pricingservice/repository/PartPriceRepository.java
+++ pricing-service/src/main/java/com/example/pricingservice/repository/PartPriceRepository.java
@@ -1,4 +1,13 @@
 package com.example.pricingservice.repository;
 
-public class PartPriceRepository {
+
+import com.example.pricingservice.entity.PartPrice;
+import jakarta.servlet.http.Part;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.OptionalInt;
+
+public interface PartPriceRepository extends JpaRepository<PartPrice, Integer> {
+    Optional<PartPrice> findByPartName(String partName);
 }
