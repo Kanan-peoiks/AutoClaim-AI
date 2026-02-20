3f746ce3c777447ba42dcfbc47b7a84ea3330d02
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-02-20 21:36:58 +0400

    feat: update claim-service client implementations

--- claim-service/src/main/java/com/example/claimservice/client/PricingClient.java
+++ claim-service/src/main/java/com/example/claimservice/client/PricingClient.java
@@ -4,8 +4,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "pricing-service", url = "http://localhost:8083")
+@FeignClient(name = "pricing-service", url = "${pricing.service.url}")
 public interface PricingClient {
+
     @GetMapping("/api/prices/{partName}")
-    double getPrice(@PathVariable String partName);
+    double getPrice(@PathVariable("partName") String partName);
 }
