170f0acae5aabd2fc700462c041ef236a169919d
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-02-20 21:37:59 +0400

    feat: update claim-service repository

--- claim-service/src/main/java/com/example/claimservice/repository/ClaimRepository.java
+++ claim-service/src/main/java/com/example/claimservice/repository/ClaimRepository.java
@@ -1,4 +1,9 @@
 package com.example.claimservice.repository;
 
-public class ClaimRepository {
+import com.example.claimservice.entity.Claim;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClaimRepository extends JpaRepository<Claim, Long> {
 }
