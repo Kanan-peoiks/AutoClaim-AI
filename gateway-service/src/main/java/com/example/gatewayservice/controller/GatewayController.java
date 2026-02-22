2d64ef924d88493de627d41dccaab458eb02b864
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-02-23 01:40:31 +0400

    feat: update gateway-service controller

--- gateway-service/src/main/java/com/example/gatewayservice/controller/GatewayController.java
+++ gateway-service/src/main/java/com/example/gatewayservice/controller/GatewayController.java
@@ -1,30 +1,39 @@
 package com.example.gatewayservice.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
+import com.example.gatewayservice.config.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
 @RequestMapping("/api/gateway")
+@RequiredArgsConstructor
 public class GatewayController {
 
-    private final Map<String, Integer> userCounter = new HashMap<>();
+    private final JwtUtil jwtUtil;
+    private final ConcurrentHashMap<String, Integer> userCounter = new ConcurrentHashMap<>();
 
-
-    @GetMapping("/forward")
-    public String forwardRequest(@RequestParam String username) {
-        userCounter.putIfAbsent(username, 0);
-        if  (userCounter.get(username) >= 3) {
-            return "Rate limit exceeded for today";
+    @GetMapping("/check-limit")
+    public ResponseEntity<String> checkLimit(@RequestHeader(value = "Authorization", required = false) String authHeader) {
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Token missing");
         }
+
+        String token = authHeader.substring(7);
+        if (!jwtUtil.validateToken(token)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid token");
+        }
+
+        String username = jwtUtil.getUsername(token);
+        userCounter.putIfAbsent(username, 0);
+
+        if (userCounter.get(username) >= 3) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).body("Limit exceeded");
+        }
+
         userCounter.put(username, userCounter.get(username) + 1);
-        // forward to claim service ?
-        return "Success";
+        return ResponseEntity.ok("Allowed. Calls left: " + (3 - userCounter.get(username)));
     }
-
-
 }
